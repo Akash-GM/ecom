66fb8e536d3f364e18595961c8f082d3aab50d4d
akash <george.akash99@gmail.com>
2025-02-24 22:35:43 +0530

    serializers,api views
    added serializers for the models also added list and detail CBVs for products

--- api/views.py
+++ api/views.py
@@ -1,1 +1,39 @@
-from django.shortcuts import render
+from api.models import Product
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
+
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    renderer_classes = [JSONRenderer]
+
+
+@api_view(["GET"])
+def product_list(request):
+    """
+    returns list of all products
+    like ProductList class
+    """
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response({"data": serializer.data})
+
+
+class ProductDetail(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+@api_view(["GET"])
+def product_detail(request, pk):
+    """
+    single product_detail
+    """
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
