cf8e94049af152b029a29502252394f2e04f2f0d
akash <george.akash99@gmail.com>
2025-02-26 14:11:54 +0530

    custom perms
    customized perms depening on the method of the req, create a client file for Rest Client

--- api/views.py
+++ api/views.py
@@ -11,7 +11,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 
 
@@ -129,3 +129,9 @@ class ProductCreateAPIView(generics.CreateAPIView):
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == "POST":
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
